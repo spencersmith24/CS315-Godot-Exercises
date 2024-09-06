0880daaf67ad31ac4f7b2dc90bf118555fd0d365
Spencer Smith <spencersmith24@outlook.com>
2024-09-06 14:45:09 -0400

    add player movement script

--- ShootEmUp/shootemup/Scripts/player.gd
+++ ShootEmUp/shootemup/Scripts/player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
-
-const SPEED = 400.0
-
+const SPEED = 500.0
 
 func _physics_process(delta):
+	var ship_direction: Vector2
 	
 	if Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-	elif Input.is_action_pressed("right"):
-		velocity.x = SPEED
-	else:
-		velocity.x = 0
-
+		ship_direction += Vector2.LEFT
+	if Input.is_action_pressed("right"):
+		ship_direction += Vector2.RIGHT
+	if Input.is_action_pressed("up"):
+		ship_direction += Vector2.UP
+	if Input.is_action_pressed("down"):
+		ship_direction += Vector2.DOWN
+	
+	velocity = ship_direction.normalized() * SPEED
 	move_and_slide()
