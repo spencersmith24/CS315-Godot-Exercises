dd4f5b39d4cbbfb1cc470dda8f02c7b48fda45b7
Spencer Smith <spencersmith24@outlook.com>
2024-09-08 11:51:40 -0400

    add score increment script to enemy resource

--- ShootEmUp/shootemup/Scripts/enemy.gd
+++ ShootEmUp/shootemup/Scripts/enemy.gd
@@ -14,5 +14,6 @@ func _process(delta):
 
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	get_parent().get_parent().increment_score()
 	queue_free()
 	body.queue_free()
