98a058ca6071bf71d484c6c4dde66c004c9dbb4f
Spencer Smith <spencersmith24@outlook.com>
2024-09-18 12:37:11 -0400

    work

---
+++ SpinnyPlane/tappyplane/scenes/plane.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var flap_dir = Vector2(0, -500)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (Input.is_action_just_pressed("flap")):
+		apply_impulse(flap_dir)
+		$AnimatedSprite2D.play("flap")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	pass
